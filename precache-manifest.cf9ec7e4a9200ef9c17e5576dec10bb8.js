61c3e0c7f358e223de914cf9ca46ac1217df8378
witold urbas <wiur83@gmail.com>
2020-03-11 22:02:21 +0100

    Updates

---
+++ precache-manifest.cf9ec7e4a9200ef9c17e5576dec10bb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bb9047c7a85f225a64f537cad39800",
+    "url": "/react-test1/index.html"
+  },
+  {
+    "revision": "ca98a1ea6b2c710e55d9",
+    "url": "/react-test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34f5470a048a810751bf",
+    "url": "/react-test1/static/js/2.8364879f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-test1/static/js/2.8364879f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca98a1ea6b2c710e55d9",
+    "url": "/react-test1/static/js/main.e8f30ef6.chunk.js"
+  },
+  {
+    "revision": "a0ee354300a1509361b6",
+    "url": "/react-test1/static/js/runtime-main.416f3bc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
